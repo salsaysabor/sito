9f0e28d1a8e2e14afcd58807a536b2b7d55e71b3
salsaysabor <infosalsaysabor@gmail.com>
2023-03-20 19:10:05 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,25 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  // https://mercurial-learning-1b7.notion.site/SALSA-Y-SABOR-TEST-a7fba22af49246c8928da2445715a066
+  rootNotionPageId: 'a7fba22af49246c8928da2445715a066',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Salsa Y Sabor Test Site',
+  domain: 'salsaysabor.vercel.app',
+  author: 'Tommaso Lovergine',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Scuola di Ballo Salsa Y Sabor',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  //github: 'transitive-bullshit',
+  //linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
